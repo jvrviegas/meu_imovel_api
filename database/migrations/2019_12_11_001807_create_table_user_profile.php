cb6914f47b8222d6c29190928b0674103ee1eb92
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2019-12-10 22:38:14 -0300

    Init project

---
+++ database/migrations/2019_12_11_001807_create_table_user_profile.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUserProfile extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('about')->nullable();
+            $table->text('social_networks')->nullable();
+            $table->string('phone');
+            $table->string('mobile_phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile');
+    }
+}
